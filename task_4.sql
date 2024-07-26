8b6cd421fc8fd66c9fdfc16c75dede4732eb0c78
Bongani Ndlovu <167146765+Ndlovu19-code@users.noreply.github.com>
2024-07-26 07:08:13 -0700

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -3,3 +3,7 @@
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM information_schema.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
+git add task_4.sql
+git commit -m "Add script to print full description of the table 'Books' in alx_book_store database"
+git push origin main
+
