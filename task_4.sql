ac1411ccb0b7b0d7204125c2b4277a9eb768087f
Bongani Ndlovu <167146765+Ndlovu19-code@users.noreply.github.com>
2024-07-26 12:35:54 -0700

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,21 +1,16 @@
 -- task_4.sql
-
-USE INFORMATION_SCHEMA;
-
 SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
 FROM 
-    COLUMNS
+    INFORMATION_SCHEMA.COLUMNS 
 WHERE 
-    TABLE_SCHEMA = DATABASE() -- This will use the database name passed in the command
-    AND TABLE_NAME = 'books'
-ORDER BY 
-    ORDINAL_POSITION;
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
 
 
 
