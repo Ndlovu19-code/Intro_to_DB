bf9cfd9a35362d4b1d4301870c318efbda6603ac
Bongani Ndlovu <167146765+Ndlovu19-code@users.noreply.github.com>
2024-07-28 13:11:20 -0700

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -6,11 +6,7 @@ SELECT
     COLUMN_KEY, 
     COLUMN_DEFAULT, 
     EXTRA 
-FROM 
-    INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books';
+
 
 
 
